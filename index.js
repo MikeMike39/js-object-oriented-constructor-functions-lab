d299e5d745e3c5483d7368a3f11bbd9b868d1c9b
MikeMike39 <Mfonseca294@gmail.com>
2021-05-05 08:15:43 -0400

    Done with assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+// Scooter constructor creates a new scooter with three properties
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+// Driver constructor creates a new driver with three properties 
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+// PickupLocation constructor creates a new PickupLocation with two properties
+function PickupLocation(address, city){
+  this.address = address;
+  this.city = city;
+}
